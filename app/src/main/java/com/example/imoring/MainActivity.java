5751d3c42fecf7079a5a8502cd0407d3b97b8022
Mazid Ahmad <mazid.ahmad.tif17@polban.ac.id>
2019-06-13 09:44:07 +0700

    Tambah Fragment

--- app/src/main/java/com/example/imoring/MainActivity.java
+++ app/src/main/java/com/example/imoring/MainActivity.java
@@ -1,13 +1,127 @@
 package com.example.imoring;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.imoring.Fragment.DashboardFragment;
+import com.example.imoring.Fragment.HomeFragment;
+import com.example.imoring.Fragment.NotificationFragment;
+import com.example.imoring.Fragment.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    //Our View Pager
+    private ViewPager viewPager;
+
+    //Fragment
+    HomeFragment homeFragment;
+    DashboardFragment dashboardFragment;
+    NotificationFragment notificationFragment;
+    ProfileFragment profileFragment;
+    MenuItem prevMenuItem;
+
+    private TextView mTextMessage;
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Inisiasi ViewPager
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        //Inisiasi Bottom Navigation Bar
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+            new BottomNavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        viewPager.setCurrentItem(0);
+                        break;
+                    case R.id.navigation_dashboard:
+                        viewPager.setCurrentItem(1);
+                        break;
+                    case R.id.navigation_notifications:
+                        viewPager.setCurrentItem(2);
+                        break;
+                    case R.id.navigation_profile:
+                        viewPager.setCurrentItem(3);
+                        break;
+                }
+                return false;
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(prevMenuItem != null){
+                    prevMenuItem.setChecked(false);
+                }
+                else{
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                Log.d("page","onPageSelected"+position);
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomNavigationView.getMenu().getItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        setupViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        homeFragment = new HomeFragment();
+        dashboardFragment = new DashboardFragment();
+        notificationFragment = new NotificationFragment();
+        profileFragment = new ProfileFragment();
+        adapter.addFragment(homeFragment);
+        adapter.addFragment(dashboardFragment);
+        adapter.addFragment(notificationFragment);
+        adapter.addFragment(profileFragment);
+        viewPager.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/imoring/SplashScreen.java
+++ app/src/main/java/com/example/imoring/SplashScreen.java
@@ -1,13 +1,25 @@
 package com.example.imoring;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
+    private int waktu_loading=2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(home);
+                finish();
+            }
+        },waktu_loading);
     }
 }
